f5c8c50dabb2a69eeddeda78e826f3d8ee81b329
KajalAgarwal26 <59644568+KajalAgarwal26@users.noreply.github.com>
2020-01-16 16:44:22 +0530

    Update New.java

--- New.java
+++ New.java
@@ -1,7 +1,7 @@
 public class New{
 public static void main(String args[])
 {
-for(i=0;i<=10;i++)
+for(int i=0;i<=10;i++)
 {
 System.out.println("This is line no." +i);
 System.out.println("This is Hello ");
