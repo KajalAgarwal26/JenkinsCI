d0f66d2d30c8470f1e908b7e1f817fc46d6c9df7
KajalAgarwal26 <59644568+KajalAgarwal26@users.noreply.github.com>
2020-01-16 16:30:42 +0530

    Create new.java

---
+++ new.java
@@ -0,0 +1,11 @@
+public class hello
+{
+public static void main(String args[])
+{
+for(i=0;i<=10;i++)
+{
+system.out.println("This is line no." +i);
+System.out.println("This is Hello program me");
+}
+}
+}
